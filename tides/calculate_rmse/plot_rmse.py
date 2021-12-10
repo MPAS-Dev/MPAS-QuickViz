6034ca87bbec80ea792c2049019cb95537c5d20e
Kristin Barton <knbarton@umich.edu>
2021-12-10 10:48:14 -0800

    Fixed errors in the Tides RMSE and plotting routine

--- tides/calculate_rmse/plot_rmse.py
+++ tides/calculate_rmse/plot_rmse.py
@@ -92,18 +92,19 @@ def main(argv):
 
     # Use these to fix up the plots
     subplot_levels = [[np.linspace(0,0.65,16), np.linspace(0,0.65,16), np.linspace(0,0.13,16), np.linspace(0,0.13,16)], \
-                      [np.linspace(0,1.4, 16),  np.linspace(0,1.4,16), np.linspace(0,0.22,16), np.linspace(0,0.22,16)], \
-                      [np.linspace(0,0.22,16), np.linspace(0,0.22,16), np.linspace(0,0.05,16),np.linspace(0,0.05, 16)], \
+                      [np.linspace(0,1.4, 16),  np.linspace(0,1.4,16), np.linspace(0,0.22,16), np.linspace(0,0.5,16)], \
+                      [np.linspace(0,0.22,16), np.linspace(0,0.22,16), np.linspace(0,0.05,16),np.linspace(0,0.025, 16)], \
                       [np.linspace(0,0.5, 16), np.linspace(0,0.5, 16), np.linspace(0,0.08,16),np.linspace(0,0.08,16)], \
                       [np.linspace(0,0.7, 16), np.linspace(0,0.7,16), np.linspace(0,0.5, 16), np.linspace(0,0.5,16)]]
 
     subplot_ticks = [[np.linspace(0,0.65, 10), np.linspace(0,0.65,10), np.linspace(0,0.13,10), np.linspace(0,0.13,10)], \
-                      [np.linspace(0,1.4, 10),  np.linspace(0,1.4,10), np.linspace(0,0.22,10), np.linspace(0,0.22,10)], \
+                      [np.linspace(0,1.4, 10),  np.linspace(0,1.4,10), np.linspace(0,0.22,10), np.linspace(0,0.25,10)], \
                       [np.linspace(0,0.22,10), np.linspace(0,0.22,10), np.linspace(0,0.05,10),np.linspace(0,0.05, 10)], \
                       [np.linspace(0,0.5, 10), np.linspace(0,0.5, 10), np.linspace(0,0.08,10),np.linspace(0,0.08,10)], \
                       [np.linspace(0,0.7, 10), np.linspace(0,0.7,10), np.linspace(0,0.5, 10), np.linspace(0,0.5,10)]]
 
-    for constituent_num in range(0,5):
+    # Calculates in order: ['K1','M2','N2','O1','S2']
+    for constituent_num in range(0,4):
         constituent = constituent_list[constituent_num]
 
         print(" ====== " + constituent + " Constituent ======")
@@ -118,57 +119,60 @@ def main(argv):
         rmse_amp = 0.5*(data1 - data2)**2
         rmse_com = 0.5*(data2**2 + data1**2) - data1*data2*np.cos(data2_phase - data1_phase)
 
-        # Get rid of WAY too large values
-        rmse_amp[:] = rmse_amp[:]*(rmse_amp[:]<100)
-        rmse_com[:] = rmse_com[:]*(rmse_com[:]<100)
-
         # Calculate mean (global) values
         rmse_amp_sum = 0
         rmse_com_sum = 0
-        areaTotal = 0
+        areaTotalAmp = 0
+        areaTotalCom = 0
         for cell in range(0,nCells-1):
-            rmse_amp_sum += rmse_amp[cell]*area[cell]
-            rmse_com_sum += rmse_com[cell]*area[cell]
-            areaTotal += area[cell]
-        global_rmse_amp = np.sqrt(rmse_amp_sum / areaTotal)
-        global_rmse_com = np.sqrt(rmse_com_sum / areaTotal)
-#        print('Global RMSE (Amp) = ', global_rmse_amp)
+            if (depth[cell] > 20) and (rmse_com[cell] < 1000) and (rmse_amp[cell] < 1000):
+                if (rmse_amp[cell] < 10000):
+                    rmse_amp_sum += rmse_amp[cell]*area[cell]
+                    areaTotalAmp += area[cell]
+                if (rmse_com[cell] < 10000):
+                    rmse_com_sum += rmse_com[cell]*area[cell]
+                    areaTotalCom += area[cell]
+        global_rmse_amp = np.sqrt(rmse_amp_sum / areaTotalAmp)
+        global_rmse_com = np.sqrt(rmse_com_sum / areaTotalCom)
+        print('Global RMSE (Amp) = ', global_rmse_amp)
         print('Global RMSE (Com) = ', global_rmse_com)
 
         # Calculate shallow RMSE (<=1000m)
-        count = 0
         rmse_amp_sum = 0
         rmse_com_sum = 0
-        areaTotal = 0
+        areaTotalAmp = 0
+        areaTotalCom = 0
         for cell in range(0,nCells-1):
-            if abs(lat_grid[cell]) < 66:
-                if (depth[cell] < 1000) and (depth[cell] > 20):
+            if (abs(lat_grid[cell]) < 66) and (depth[cell] < 1000) and (depth[cell] > 20):
+                if (rmse_amp[cell] < 10000):
                     rmse_amp_sum += rmse_amp[cell]*area[cell]
+                    areaTotalAmp += area[cell]
+                if (rmse_com[cell] < 10000):
                     rmse_com_sum += rmse_com[cell]*area[cell]
-                    areaTotal += area[cell]
-        shallow_rmse_amp = np.sqrt(rmse_amp_sum / areaTotal)
-        shallow_rmse_com = np.sqrt(rmse_com_sum / areaTotal)
-#        print('Shallow RMSE (Amp) = ', shallow_rmse_amp)
+                    areaTotalCom += area[cell]
+        shallow_rmse_amp = np.sqrt(rmse_amp_sum / areaTotalAmp)
+        shallow_rmse_com = np.sqrt(rmse_com_sum / areaTotalCom)
+        print('Shallow RMSE (Amp) = ', shallow_rmse_amp)
         print('Shallow RMSE (Com) = ', shallow_rmse_com)
 
         # Calculate deep RMSE (>1000m)
-        count = 0
         rmse_amp_sum = 0
         rmse_com_sum = 0
         areaTotal = 0
         for cell in range(0,nCells-1):
-            if abs(lat_grid[cell]) < 66:
-                if depth[cell] >= 1000:
-                    count += 1
-                    rmse_amp_sum += rmse_amp[cell]*area[cell]
-                    rmse_com_sum += rmse_com[cell]*area[cell]
-                    areaTotal += area[cell]
+            if (abs(lat_grid[cell]) < 66) and (depth[cell] >= 1000):
+                rmse_amp_sum += rmse_amp[cell]*area[cell]
+                rmse_com_sum += rmse_com[cell]*area[cell]
+                areaTotal += area[cell]
         deep_rmse_amp = np.sqrt(rmse_amp_sum / areaTotal)
         deep_rmse_com = np.sqrt(rmse_com_sum / areaTotal)
-#        print('Deep RMSE (Amp) = ', deep_rmse_amp)
+        print('Deep RMSE (Amp) = ', deep_rmse_amp)
         print('Deep RMSE (Com) = ', deep_rmse_com)
 
-#        # Plot data
+        rmse_amp = rmse_amp**0.5
+        rmse_com = rmse_com**0.5
+
+        # Plot data
         fig=plt.figure(figsize=(18,12))
         subplot_title = [constituent+' Amplitude (simulation) [m]', constituent+' Amplitude (TPXO8) [m]', \
                          constituent+' RMSE (Amplitude) [m]', constituent+' RMSE (Complex) [m]']
@@ -203,8 +207,8 @@ def main(argv):
 
         fig.tight_layout()
         fig.suptitle(resolution + ' with run type: ' + saltype + '\n' +
-                        'Complex: Global Avg = ' + str(round(global_rmse_com*100,3)) + 'cm' + \
-                        '; Deep RMSE = ' + str(round(deep_rmse_com*100,3)) + 'cm' + \
+                        'Complex: Global Avg = ' + str(round(global_rmse_com*100,3)) + 'cm' + ';'\
+                        ' Deep RMSE = ' + str(round(deep_rmse_com*100,3)) + 'cm' + \
                         '; Shallow RMSE = ' + str(round(shallow_rmse_com*100,3)) + 'cm', \
                         fontsize=20)
         plt.savefig(constituent+'_plot.png')
