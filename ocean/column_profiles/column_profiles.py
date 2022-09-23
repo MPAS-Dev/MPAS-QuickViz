1bf8f97e45c42633b49bcc5a908cdd8e1b1a4246
Mark Petersen <mpetersen@lanl.gov>
2022-09-22 20:03:32 -0600

    Finished cell plots

--- ocean/column_profiles/column_profiles.py
+++ ocean/column_profiles/column_profiles.py
@@ -7,9 +7,9 @@ September 2022
 
 ############################## model files, run dirs
 runDir = '/lustre/scratch5/turquoise/mpeterse/runs/s/'
-simName = 's03g/'
-fileName = 'output.nc'
-meshName = 'init.nc'
+simName = 's03m'
+fileName = '/output.nc'
+meshName = '/init.nc'
 
 lonMin = 0.193
 lonMax = 0.210
@@ -18,20 +18,20 @@ latMax = 0.490
 
 #from __future__ import absolute_import, division, print_function, \
 #    unicode_literals
-import os
-import glob
+#import os
+#import glob
 import matplotlib as mpl
-mpl.use('Agg')
+#mpl.use('Agg')
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.colors as cols
-from matplotlib.pyplot import cm
-from matplotlib.colors import BoundaryNorm
-import cmocean
+#import matplotlib.colors as cols
+#from matplotlib.pyplot import cm
+#from matplotlib.colors import BoundaryNorm
+#import cmocean
 import xarray as xr
 from netCDF4 import Dataset
-from mpas_analysis.shared.io.utility import decode_strings
-import gsw
+#from mpas_analysis.shared.io.utility import decode_strings
+#import gsw
 
 print('reading data from:')
 print(runDir+simName+meshName)
@@ -49,9 +49,10 @@ maxLevelCell = mesh.variables['maxLevelCell']
 
 cellList = np.where(np.logical_and(np.logical_and(np.logical_and(latCell>latMin, latCell<latMax), lonCell>lonMin), lonCell<lonMax))[0]
 
-iTime = 3
+iTime = 0
 fig = plt.figure(figsize=(20,12))
 varNames = ['divergence','vertVelocityTop','vertTransportVelocityTop','temperature', 'salinity','density','pressure','zMid']
+varNames = ['temperature', 'salinity']
 varNames = ['divergence','vertVelocityTop','temperature', 'salinity']
 for j in range(len(varNames)):
     plt.subplot(2,2,j+1)
@@ -61,11 +62,12 @@ for j in range(len(varNames)):
         k = int(maxLevelCell[iCell])
         varData = var[iTime,iCell,0:k]
         #varData = np.where(varData>-1e20,varData,np.NAN)
-        plt.plot(varData,np.arange(1,k+1))
+        plt.plot(varData,np.arange(1,k+1),label='cell '+str(iCell))
     plt.gca().invert_yaxis()
     plt.title(varNames[j])
     plt.grid()
+    plt.legend()
 
-figfile = 'test.png'
+figfile = 'vert_profiles_' +simName+ '.png'
 plt.savefig(figfile, bbox_inches='tight')
 plt.close()
