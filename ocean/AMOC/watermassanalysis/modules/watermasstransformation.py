9051b23afe7da67e32e77664676c6a563f0ff10b
bmooremaley <ben.mooremaley@gmail.com>
2023-08-17 16:33:21 -0700

    Added new variable definitions to watermasstransformation.py and implemented pyremap bilinear interpolation

--- ocean/AMOC/watermassanalysis/modules/watermasstransformation.py
+++ ocean/AMOC/watermassanalysis/modules/watermasstransformation.py
@@ -13,133 +13,8 @@ import os
 import sys
 import yaml
 from datetime import datetime
-from dateutil.parser import parse
 from fastjmd95 import rho, drhodt, drhods
 from tqdm import tqdm
-
-
-def load_flux_definitions():
-    """Flux definitions for MPAS-Ocean
-        TODO: include `seaIceSalinityFlux`
-    """
-
-    # Flux definitions (positive into the ocean)
-    fluxdefs = {
-        'heat': {
-            'fluxes': [                 #### - Air-sea heat fluxes ----------- #######
-                'shortWaveHeatFlux',    # Short wave radiation flux ---------- [W m-2]
-                'longWaveHeatFluxUp',   # Upward long wave heat flux --------- [W m-2]
-                'longWaveHeatFluxDown', # Downward long wave heat flux ------- [W m-2]
-                'latentHeatFlux',       # Latent heat flux ------------------- [W m-2]
-                'sensibleHeatFlux',     # Sensible heat flux ----------------- [W m-2]
-            ],
-            'meltfluxes': [             #### - Latent heat of melting fluxes - #######
-                'snowFlux',             # Fresh water flux from snow --------- [kg m-2 s-1]
-                'iceRunoffFlux',        # Fresh water flux from ice runoff --- [kg m-2 s-1]
-            ],
-        },
-        'heat_ice': {
-            'fluxes': [                 #### - Ice-ocean heat fluxes --------- #######
-                'seaIceHeatFlux',       # Sea ice heat flux ------------------ [W m-2]
-            ],
-            'meltfluxes': [             #### - Latent heat of melting fluxes - #######
-                'seaIceFreshWaterFlux', # Fresh water flux from sea ice ------ [kg m-2 s-1]
-            ],
-        },
-        'salt': {
-            'fluxes': [                 #### - Air-sea freshwater fluxes ----- #######
-                'evaporationFlux',      # Evaporation flux ------------------- [kg m-2 s-1]
-                'rainFlux',             # Fresh water flux from rain --------- [kg m-2 s-1]
-                'snowFlux',             # Fresh water flux from snow --------- [kg m-2 s-1]
-                'riverRunoffFlux',      # Fresh water flux from river runoff - [kg m-2 s-1]
-                'iceRunoffFlux',        # Fresh water flux from ice runoff --- [kg m-2 s-1]
-            ],
-        },
-        'salt_ice': {
-            'fluxes': [                 #### - Ice-ocean freshwater fluxes --- #######
-                'seaIceFreshWaterFlux', # Fresh water flux from sea ice ------ [kg m-2 s-1]
-            ],
-        },
-    }
-    
-    # Flatten to list of unique flux variable names
-    fluxnames = [component for flux in fluxdefs.values() for component in flux.values()]
-    fluxnames = list(np.unique(np.hstack(fluxnames)))
-
-    return fluxdefs, fluxnames
-
-
-def build_sigma_bins(sigmarange, binsize):
-    """Build sigma classes array
-    """
-    
-    # Build sigma classes array
-    nbins = int(abs(np.subtract(*sigmarange)) / binsize) + 1
-    sigmabins = np.arange(nbins) * binsize + sigmarange[0]
-    
-    return sigmabins
-
-
-def aggregate_fluxes(results, fluxdef):
-    """Wrapper for summing flux variables according to those listed in fluxdef.
-    Also accounts for latent heat of fusion for melt fluxes.
-    """
-    
-    # Latent heat of fusion
-    L_fusion = -3.337e5
-    
-    # Aggregate direct fluxes
-    flux = sum([results[name] for name in fluxdef['fluxes']])
-    
-    # Aggregate indirect heat fluxes via melting
-    if 'meltfluxes' in fluxdef:
-        flux = flux + sum([results[name] for name in fluxdef['meltfluxes']]) * L_fusion
-    
-    return flux
-
-
-def calc_buoyancy_fluxes(results, fluxdefs, P=0):
-    """Calculate buoyancy fluxes following according to
-    Jeong et al (2020), J Clim
-    
-    State Equation from Jackett and McDougal 1995
-    Use package fastjmd95 by R. Abernathey and J. Busecke
-
-         `pip install fastjmd95`
-    """    
-    
-    # Define constants
-    rho_0 = 1026    # --- MPAS value confirmed
-    cpsw = 3.996e3  # --- MPAS value confirmed
-
-    # Calculate state variables
-    S, T = [results[name] for name in ('salinity', 'temperature')]
-    sigma = rho(S, T, P) - 1000
-    bfactors = {
-        'salt': -drhods(S, T, P) / rho_0 * S,
-        'heat': drhodt(S, T, P) / rho_0 / cpsw,
-    }
-    
-    # Loop through flux categories
-    bfluxes = {}
-    for ctgy in fluxdefs:
-        
-        # Calculate buoyancy flux
-        coeff = bfactors[ctgy.replace('_ice', '')]
-        bfluxes[ctgy] = coeff * aggregate_fluxes(results, fluxdefs[ctgy])
-
-    return bfluxes, sigma
-
-
-def calc_tr(bflux, area, mask, binsize):
-    """Calculate water mass transformation per unit density
-    using area integral
-    """
-    
-    # Calculate transformation term F (in Sv)
-    F = np.sum(bflux[mask] * area[mask]) / binsize * 1e-6
-    
-    return F
 
 
 def build_time_array(paths):
@@ -178,6 +53,24 @@ def build_MPASO_filename(date, dateranges, paths, startyear=1946):
     return filename
 
 
+def load_variable_definitions(
+    vardefsfile='../yaml/variable_definitions.yaml',
+):
+    """Variable definitions for MPAS-Ocean
+    """
+
+    # Open yaml file and load fluxdefs dict
+    with open(vardefsfile, 'r') as f:
+        vardefs = yaml.safe_load(f)
+    
+    # Flatten vardefs to get unique varnames list
+    varnames = [name for names in vardefs.values() for name in names]
+    varnames = list(np.unique(np.hstack(varnames)))
+    vardefs.pop('other')
+
+    return varnames, vardefs
+
+
 def load_MPASO_mesh(paths):
     """Load MPAS-Ocean mesh variables needed for calculating water mass
     transformation/formation. Use `'maxLevelCell` for landmask if needed.
@@ -210,7 +103,7 @@ def load_MPASO_mesh(paths):
     return coords, subdomain
 
 
-def load_MPASO_results(resultsfile, fluxnames, subdomain, prefix='timeMonthly_avg_'):
+def load_MPASO_results(resultsfile, varnames, subdomain, prefix='timeMonthly_avg_'):
     """Load MPAS-Ocean results variables needed for calculating water mass
     transformation/formation. Use `'maxLevelCell` for landmask if needed.
     """
@@ -219,7 +112,7 @@ def load_MPASO_results(resultsfile, fluxnames, subdomain, prefix='timeMonthly_av
     with xr.open_dataset(resultsfile) as ds:
         
         # Load flux variables
-        results = {name: ds[prefix + name][0, :].values[subdomain] for name in fluxnames}
+        results = {name: ds[prefix + name][0, :].values[subdomain] for name in varnames}
         
         # Load surface tracers
         for name in ['salinity', 'temperature']:
@@ -228,35 +121,122 @@ def load_MPASO_results(resultsfile, fluxnames, subdomain, prefix='timeMonthly_av
     return results
 
 
+def build_sigma_bins(sigmarange, binsize):
+    """Build sigma classes array
+    """
+    
+    # Build sigma classes array
+    nbins = int(abs(np.subtract(*sigmarange)) / binsize) + 1
+    sigmabins = np.arange(nbins) * binsize + sigmarange[0]
+    
+    return sigmabins
+
+
+def calc_state_variables(S, T, P=0):
+    """State Equation from Jackett and McDougal 1995
+    Use package fastjmd95 by R. Abernathey and J. Busecke
+
+         `pip install fastjmd95`
+    """
+
+    # Define constants
+    rho0 = 1026.0     # Seawater density constant [kg m-3]
+    cpsw = 3.996e3    # Heat capacity of seawater [J kg-1 K-1]
+
+    # Calculate state variables
+    statevars = {
+        'sigma': rho(S, T, P) - 1000,
+        'heat_factor': drhodt(S, T, P) / rho0 / cpsw,
+        'salt_factor': -drhods(S, T, P) / rho0 * S,
+    }
+
+    return statevars
+
+
+def calc_buoyancy_fluxes(results, fluxdefs):
+    """Calculate buoyancy fluxes according to fluxdefs
+    Also accounts for latent heat of fusion for melt fluxes.
+    """
+    
+    # Latent heat of fusion [J kg-1]
+    L_fusion = -3.337e5
+    
+    # Init buoyancy flux dict
+    bfluxes = {}
+    
+    # Heat flux contributions
+    for name in fluxdefs['heat']:
+        bfluxes[name] = results['heat_factor'] * results[name]
+    
+    # Heat flux contributions due to melting/freezing
+    for name in fluxdefs['melt']:
+        name_appended = name.replace('Flux', 'HeatFlux')
+        bfluxes[name_appended] = results['heat_factor'] * results[name] * L_fusion
+    
+    # Salt flux contributions
+    for name in fluxdefs['salt']:
+        bfluxes[name] = results['salt_factor'] * results[name]
+    
+    return bfluxes
+
+
+def calc_transformation(bflux, area, mask, binsize):
+    """Calculate water mass transformation per unit density
+    using area integral
+    """
+    
+    # Calculate transformation term F (in Sv)
+    F = np.sum(bflux[mask] * area[mask]) / binsize * 1e-6
+    
+    return F
+
+
 def write_output(mpasvars, wmtrvars, coords, paths):
     """Write wmtf results to netCDF. Filename conventions are taken from
     the results file prefix and the daterange
     """
-
+    
     # Filename strings
-    prefix = paths['prefix'].split('.')[0]
+    mesh = paths['meshfile'].split('/')[-2]
+    prefix = '_'.join(paths['prefix'].split('_')[:3])
     datestr = '_'.join(time.strftime('%Y%m%d') for time in coords['time'][[0, -1]])
     
-    # Write MPAS variables to netCDF
-    filename = os.path.join(paths['out'], f'{prefix}.mpasvars_{datestr}.nc')
-    coordinates = {name: (name, coords[name]) for name in ['time', 'nCells', 'regionNames']}
-    datavars = {name: ('nCells', coords[name]) for name in ['lon', 'lat', 'area']}
-    datavars['regionMasks'] = (['nCells', 'regionNames'], coords['regionMasks'])
-    datavars.update({name: (['time', 'nCells'], np.vstack(mpasvars[name])) for name in mpasvars})
-    ds = xr.Dataset(datavars, coordinates)
-    ds.to_netcdf(filename)
+    # ---- Write MPAS variables to netCDF --------
+    coordinates = {
+        'time'       : ('time', coords['time']),
+        'nCells'     : ('nCells', coords['nCells']),
+        'regionNames': ('regionNames', coords['regionNames']),
+    }
+    datavars = {
+        'lon'        : ('nCells', coords['lon']),
+        'lat'        : ('nCells', coords['lat']),
+        'area'       : ('nCells', coords['area']),
+        'regionMasks': (['nCells', 'regionNames'], coords['regionMasks']),
+    }
+    datavars.update(
+        {name: (['time', 'nCells'], np.vstack(mpasvars[name])) for name in mpasvars}
+    )
+    filename = os.path.join(paths['out'], f'{prefix}_{mesh}.mpasvars_{datestr}.nc')
+    xr.Dataset(datavars, coordinates).to_netcdf(filename)
+    # --------------------------------------------
 
-    # Write wmtr variables to netCDF
-    filename = os.path.join(paths['out'], f'{prefix}.wmtrvars_{datestr}.nc')
-    dims = ['time', 'regionNames', 'sigmaBins']
+    # ---- Write wmtr variables to netCDF --------
+    coordinates = {
+        'time'       : ('time', coords['time']),
+        'regionNames': ('regionNames', coords['regionNames']),
+        'sigmaBins'  : ('sigmaBins', coords['sigmaBins']),
+    }
+    dims = list(coordinates)
     shape = [len(coords[name]) for name in dims]
-    coordinates = {name: (name, coords[name]) for name in dims}
-    datavars = {name: (dims, np.array(wmtrvars[name]).reshape(shape)) for name in wmtrvars}
-    ds = xr.Dataset(datavars, coordinates)
-    ds.to_netcdf(filename)
+    datavars = (
+        {name: (dims, np.array(wmtrvars[name]).reshape(shape)) for name in wmtrvars}
+    )
+    filename = os.path.join(paths['out'], f'{prefix}_{mesh}.wmtrvars_{datestr}.nc')
+    xr.Dataset(datavars, coordinates).to_netcdf(filename)
+    # --------------------------------------------
 
 
-def run_wmtr(pathsfile, sigmarange=[19, 29], binsize=0.1):
+def run_wmtr(pathsfile, sigmarange=[19, 29], binsize=0.05):
     """Run the water mass transformation code for years and paths dict specified.
     `paths` must include `results`, `prefix`, `meshfile` and `maskfile` fields
     """
@@ -266,41 +246,57 @@ def run_wmtr(pathsfile, sigmarange=[19, 29], binsize=0.1):
         paths = yaml.safe_load(f)
 
     # Filenames, mesh variables, sigma bins, flux definitions
-    fluxdefs, fluxnames = load_flux_definitions()
+    varnames, fluxdefs = load_variable_definitions()
     coords, subdomain = load_MPASO_mesh(paths)
     coords['time'], dateranges = build_time_array(paths['results'])
     coords['sigmaBins'] = build_sigma_bins(sigmarange, binsize)
 
     # Initialize storage dictionaries
-    mpasvars = {name: [] for name in fluxnames + ['salinity', 'temperature']}
-    wmtrvars = {name: [] for name in fluxdefs}
+    mpasvars, wmtrvars = {}, {}
 
     # Loop through filenames
     for date in tqdm(coords['time']):
         
-        # Load results and append to list
+        # Load results
         filename = build_MPASO_filename(date, dateranges, paths)
-        results = load_MPASO_results(filename, fluxnames, subdomain)
+        results = load_MPASO_results(filename, varnames, subdomain)
+        
+        # Calculate state variables and append to results
+        statevars = calc_state_variables(results['salinity'], results['temperature'])
+        results.update(statevars)
+        
+        # Append results to list
         for name in results:
+            if name not in mpasvars:
+                mpasvars[name] = []
             mpasvars[name].append(results[name])
         
         # Calculate buoyancy fluxes
-        bfluxes, sigma = calc_buoyancy_fluxes(results, fluxdefs)
+        bfluxes = calc_buoyancy_fluxes(results, fluxdefs)
 
         # Loop through regions
         for rmask in coords['regionMasks'].T:
 
             # Apply region mask to flux variables
             bf_r = {name: bfluxes[name][rmask] for name in bfluxes}
-            sigma_r, area_r = sigma[rmask], coords['area'][rmask]
+            sigma_r, area_r = results['sigma'][rmask], coords['area'][rmask]
 
             # Loop through density bins
             for sigmabin in coords['sigmaBins']:
 
-                # Create density mask and calculate transformation term F (area integral)
+                # Create density mask
                 dmask = (sigma_r >= sigmabin) & (sigma_r <= sigmabin + binsize)
-                for name in fluxdefs:
-                    wmtrvars[name].append(calc_tr(bf_r[name], area_r, dmask, binsize))
+                
+                # Loop through buoyancy flux contributions
+                for name in bf_r:
+                    
+                    # Calculate transformation term F (area integral over dmask)
+                    F = calc_transformation(bf_r[name], area_r, dmask, binsize)
+                    
+                    # Append buoyancy flux contribution to list
+                    if name not in wmtrvars:
+                        wmtrvars[name] = []
+                    wmtrvars[name].append(F)
 
     # Save output
     write_output(mpasvars, wmtrvars, coords, paths)
