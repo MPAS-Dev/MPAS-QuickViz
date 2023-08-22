b064272d8eced0cf9448c4aa6e6839311ccfd2bf
bmooremaley <ben.mooremaley@gmail.com>
2023-08-22 16:59:21 -0700

    Added more spatial fields to water mass analysis including 2d formation field, and broke down analysis by month

--- ocean/AMOC/watermassanalysis/modules/visualizationtools.py
+++ ocean/AMOC/watermassanalysis/modules/visualizationtools.py
@@ -10,8 +10,9 @@ import numpy as np
 import xarray as xr
 import os
 import pyremap
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, patches
 from tqdm import tqdm
+import watermasstransformation as wmt
 
 
 def parse_contour_levels(clims):
@@ -43,7 +44,7 @@ def parse_contour_levels(clims):
 
 
 def build_remapper(
-    mesh, varnames,
+    mesh,
     mesh_path='/global/cfs/cdirs/e3sm/inputdata/ocn/mpas-o/',
     mapping_path='/global/cfs/cdirs/m4259/mapping_files/',
 ):
@@ -73,10 +74,20 @@ def build_remapper(
     return remapper, da_full
 
 
-def build_variables_spatial(ds_in, varnames, timeranges, bbox=None, months=None):
+def build_variables_spatial(ds_in, timeranges, bbox=None, months=None):
     """Prebuild plotting variables to make plotting faster. Uses `pyremap` for
     remapping to lon lat.
     """
+    
+    # Aggregate variable definitions
+    _, fluxdefs = wmt.load_variable_definitions()
+    aggregate_defs = {
+        'radiative': ['shortWaveHeatFlux', 'longWaveHeatFluxUp', 'longWaveHeatFluxDown'],
+        'precipitation': ['rainFlux', 'snowFlux'],
+        'runoff': ['riverRunoffFlux', 'iceRunoffFlux'],
+        'total_heat': fluxdefs['heat'],
+        'total_fresh': fluxdefs['salt'],
+    }
     
     # Define lon lat xarray slicing args if bbox is provided
     if bbox is not None:
@@ -85,11 +96,15 @@ def build_variables_spatial(ds_in, varnames, timeranges, bbox=None, months=None)
     # Initialize output dict
     ds_lonlat = {mesh: {} for mesh in ds_in}
     
+    # Parse variable names
+    mesh = list(ds_in)[0]
+    varnames = [name for name in ds_in[mesh] if ds_in[mesh][name].dims == ('time', 'nCells')]
+    
     # Loop through meshes
     for mesh in ds_in:
         
         # Build remapper objects
-        remapper, da_full = build_remapper(mesh, varnames)
+        remapper, da_full = build_remapper(mesh)
         
         # Loop through timeranges
         for timerange in timeranges:
@@ -104,15 +119,30 @@ def build_variables_spatial(ds_in, varnames, timeranges, bbox=None, months=None)
                 tindex = [month in months for month in ds_tslc.time.dt.month]
                 ds_tslc = ds_tslc.sel(time=tindex)
             
+            # Compute aggregate variables
+            aggregates = {}
+            for aggregate, names in aggregate_defs.items():
+                aggregates[aggregate] = sum([ds_tslc[name] for name in names])
+            ds_tslc = ds_tslc.assign(aggregates)
+            
             # Load variables on subdomain into full domain
             cellindex = ds_tslc.nCells.values
-            for name in varnames:
+            for name in varnames + list(aggregate_defs):
                 da_full.loc[{'nCells': cellindex}] = ds_tslc[name].mean(dim='time')
                 ds_lonlat[mesh][tstring][name] = remapper.remap(da_full)
             
             # Slice according to bbox
             if bbox is not None:
                 ds_lonlat[mesh][tstring] = ds_lonlat[mesh][tstring].sel(**bbox_args)
+            
+            # Compute formation
+            names = ['lat', 'sigma', 'heat_factor', 'salt_factor', 'total_heat', 'total_fresh']
+            lat, sigma, hf, sf, th, tf = [ds_lonlat[mesh][tstring][name] for name in names]
+            area = np.ones(sigma.shape) * (111e3 * 0.5)**2 * np.cos(np.deg2rad(lat.values))[:, None]
+            dTdx, dTdy = np.gradient(hf * th + sf * tf)
+            drhodx, drhody = np.gradient(sigma)
+            formation = -(dTdx * drhodx + dTdy * drhody) / (drhodx**2 + drhody**2) * area
+            ds_lonlat[mesh][tstring] = ds_lonlat[mesh][tstring].assign({'formation': (['lat', 'lon'], formation)})
     
     return ds_lonlat
 
@@ -146,7 +176,7 @@ def plot_variable_spatial(ds, varname, units, scale=1, clims=None, cmap=None):
         
         # Print time range
         row[0].text(0.01, 0.8, tstr, transform=row[0].transAxes)
-    
+        
         # Loop through meshes
         for ax, mesh in zip(row, meshes):
             
@@ -155,24 +185,34 @@ def plot_variable_spatial(ds, varname, units, scale=1, clims=None, cmap=None):
             lon, lat, sigma, variable = [ds[mesh][tstr][name] for name in names]
 
             # Plot variable
-            c1 = ax.contourf(lon, lat, variable * scale, levels=levels1, cmap=cmap)
+            c1 = ax.contourf(lon, lat, variable * scale, levels=levels1, extend='both', cmap=cmap)
 
             # Plot sigma contours
             cs = ax.contour(lon, lat, sigma, **sigma_levels)
         
         # Plot residual
         residual = np.subtract(*[ds[mesh][tstr][varname] for mesh in meshes])
-        c2 = row[2].contourf(lon, lat, residual * scale, levels=levels2, cmap='RdBu_r')
+        c2 = row[2].contourf(lon, lat, residual * scale, levels=levels2, extend='both', cmap='RdBu_r')
     
-    # Remove ticks
+    # Gray background and remove ticks
     for ax in axs.ravel():
+        ax.set_xlim(-100, 20)
+        ax.set_ylim(0, 80)
+        ax.add_patch(patches.Rectangle([0, 0], 1, 1, fc='gray', transform=ax.transAxes, zorder=-10))
         ax.xaxis.set_ticks([])
         ax.yaxis.set_ticks([])
 
     # Add colorbars
-    cax1 = fig.add_axes([0.13, 0.07, 0.5, 0.015])
-    cax2 = fig.add_axes([0.67, 0.07, 0.21, 0.015])
+    cax1 = fig.add_axes([0.13, 0.05, 0.5, 0.015])
+    cax2 = fig.add_axes([0.65, 0.05, 0.25, 0.015])
     fig.colorbar(c1, cax=cax1, label=units, ticks=ticks1, orientation='horizontal')
     fig.colorbar(c2, cax=cax2, label=units, ticks=ticks2, orientation='horizontal')
+    for x, mesh in zip([0.9, 0.1], meshes):
+        cax2.text(x, 1.5, mesh, transform=cax2.transAxes, ha='center')
+    
+    # Add sigma contour legend
+    for level, ls, color in zip(*list(sigma_levels.values())):
+        axs[1, 0].plot(0, 0, ls, color=color, label=level)
+    axs[1, 0].legend(ncols=7, loc=(0.08, -0.1))
     
     return fig, axs
