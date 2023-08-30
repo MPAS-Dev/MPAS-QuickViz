9f37fd4fdb28b31382c4759fc80b9a5578c96077
bmooremaley <ben.mooremaley@gmail.com>
2023-08-30 13:34:13 -0700

    Added 2D seasonal formation fields for water mass analysis

--- ocean/AMOC/watermassanalysis/modules/visualizationtools.py
+++ ocean/AMOC/watermassanalysis/modules/visualizationtools.py
@@ -12,57 +12,79 @@ from matplotlib import pyplot as plt, patches
 from tqdm import tqdm
 
 
-def parse_contour_levels(clims):
-    """Parse `clims` params into arrays for setting contour levels and ticks.
-    
-    clims = [lower, upper, contour_inc, cbar_inc, residual_bound, contour_inc, cbar_inc]
+def parse_plot_args(ds_in, months, sigmabins, clims):
+    """
     """
     
-    # Return None if clims is None
+    # Default values range
+    range_def = [None, None]
+    
+    # Parse meshes and timestrings
+    meshes = list(ds_in)
+    timestrings = list(ds_in[meshes[0]])[::-1]
+    
+    # Parse months and sigmabins into slice args
+    # name: (aggregation_function, values)
+    sliceargs = {
+        'months'   : ('mean', slice(*range_def) if months is None else months),
+        'sigmaBins': ('sum' , slice(*range_def) if sigmabins is None else sigmabins),
+    }
+    
+    # Parse clims
     if clims is None:
-        return None, None, None, None
-    
-    # Else parse contour levels and ticks
+        clims = [range_def, range_def]
     else:
-        
-        # Increment to make upper bound inclusive
-        i = 1e-6
+        clims = [clims[:2], [-clims[2], clims[2]]]
     
-        # Left panel
-        l, u = clims[:2]
-        levels1, ticks1 = [np.arange(l, u + i, d) for d in clims[2:4]]
-
-        # Right panel
-        u = clims[4]
-        levels2, ticks2 = [np.arange(l, u + i, d) for l, d in zip([-u, 0], clims[5:])]
-        ticks2 = np.unique(np.hstack([-ticks2, ticks2]))
-
-    return levels1, levels2, ticks1, ticks2
+    return meshes, timestrings, sliceargs, clims
 
 
-def plot_variable_spatial(ds_in, varname, units, scale=1, month=None, clims=None, cmap=None):
-    """Plot specified variable over spatial region. Hard-coded for two timeranges,
-    two meshes and the residual between meshes. These categories must be consistent
-    with the structure of the `plotvars` dictionary.
+def plot_2Dpanel(
+    ax, ds, varname=None, scale=1, xlim=[-100, 20], ylim=[0, 80],
+    clim=[None, None], cmap=None, plot_sigma=False,
+):
+    """Plot 2D variable given by `varname` and overlay sigma
+    contours if requested.
     """
     
-    if month is None:
-        month = slice(None, None)
-    
-    # General definitions
-    meshes, tstrs = list(ds_in), list(list(ds_in.values())[0])[::-1]
-    
-    # Sigma contour level specs
+    # Sigma contour styling
     sigma_levels = {
         'levels'    : np.arange(25, 28, 0.5),
         'linestyles': ['-', '--', '-', '--', '-', '--'],
         'colors'    : ['k', 'k', 'gray', 'gray', 'lightgray', 'lightgray'],
     }
     
-    # Make plot area
-    fig, axs = plt.subplots(2, 3, figsize=(12, 8), gridspec_kw={'hspace': 0.05, 'wspace': 0.05})
+    # Plot variable
+    variable = ds[varname] if varname is not None else ds
+    c = ax.pcolormesh(ds.lon, ds.lat, variable * scale, vmin=clim[0], vmax=clim[1], cmap=cmap)
+
+    # Plot sigma contours and add dummy plot for legend
+    if plot_sigma:
+        ax.contour(ds.lon, ds.lat, ds.sigmaTheta, **sigma_levels)
+        for level, ls, color in zip(*list(sigma_levels.values())):
+            ax.plot(0, 0, ls, color=color, label=level)
     
-    # Add titles
+    # Formatting
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    ax.add_patch(patches.Rectangle([0, 0], 1, 1, fc='gray', transform=ax.transAxes, zorder=-10))
+    ax.xaxis.set_ticks([])
+    ax.yaxis.set_ticks([])
+
+    return c, variable
+
+
+def plot_2Dvariable(ds_in, varname, units, scale=1, months=None, sigmabins=None, clims=None, cmap=None):
+    """Plot specified variable over spatial region. Hard-coded for two timeranges,
+    two meshes and the residual between meshes. These categories must be consistent
+    with the structure of the `plotvars` dictionary.
+    """
+    
+    # Parse plot arguments
+    meshes, tstrs, sliceargs, clims = parse_plot_args(ds_in, months, sigmabins, clims)
+    
+    # Make plot area and add titles
+    fig, axs = plt.subplots(2, 3, figsize=(12, 8), gridspec_kw={'hspace': 0.05, 'wspace': 0.05})
     for col, title in zip(axs.T, meshes + [f'{meshes[0]}-{meshes[1]}']):
         col[0].set_title(title)
     
@@ -72,36 +94,25 @@ def plot_variable_spatial(ds_in, varname, units, scale=1, month=None, clims=None
         # Print time range
         row[0].text(0.01, 0.8, tstr, transform=row[0].transAxes)
         
-        # Loop through meshes
-        residual = []
+        # --- Plot meshes ---------
+        res = []
         for ax, mesh in zip(row, meshes):
             
-            # Select months
-            ds = ds_in[mesh][tstr].sel(months=month).mean(dim='months')
+            # Slice dataset
+            ds = ds_in[mesh][tstr]
+            for name, values in sliceargs.items():
+                if name in ds.dims:
+                    ds = ds.sel(**{name: values[1]})
+                if name in ds.dims:
+                    ds = getattr(ds, values[0])(dim=name, skipna=False)
             
-            # Extract variables
-            names = ('lon', 'lat', 'sigmaTheta', varname)
-            lon, lat, sigma, variable = [ds[name] for name in names]
-
-            # Plot variable
-            c1 = ax.pcolormesh(lon, lat, variable * scale, vmin=clims[0], vmax=clims[1], cmap=cmap)
-
-            # Plot sigma contours
-            cs = ax.contour(lon, lat, sigma, **sigma_levels)
-            
-            residual.append(variable)
+            # Plot sliced dataset
+            c1, variable = plot_2Dpanel(ax, ds, varname, scale=scale, clim=clims[0], cmap=cmap, plot_sigma=True)
+            res.append(variable)
         
-        # Plot residual
-        residual = np.subtract(*residual)
-        c2 = row[2].pcolormesh(lon, lat, residual * scale, vmin=-clims[2], vmax=clims[2], cmap='RdBu_r')
-    
-    # Gray background and remove ticks
-    for ax in axs.ravel():
-        ax.set_xlim(-100, 20)
-        ax.set_ylim(0, 80)
-        ax.add_patch(patches.Rectangle([0, 0], 1, 1, fc='gray', transform=ax.transAxes, zorder=-10))
-        ax.xaxis.set_ticks([])
-        ax.yaxis.set_ticks([])
+        # --- Plot residual -------
+        res = np.subtract(*res)
+        c2, _ = plot_2Dpanel(row[2], res, scale=scale, clim=clims[1], cmap='RdBu_r')
 
     # Add colorbars
     cax1 = fig.add_axes([0.14, 0.05, 0.48, 0.015])
@@ -112,8 +123,6 @@ def plot_variable_spatial(ds_in, varname, units, scale=1, month=None, clims=None
         cax2.text(x, 1.5, mesh, transform=cax2.transAxes, ha='center')
     
     # Add sigma contour legend
-    for level, ls, color in zip(*list(sigma_levels.values())):
-        axs[1, 0].plot(0, 0, ls, color=color, label=level)
     axs[1, 0].legend(ncols=7, loc=(0.08, -0.1))
     
     return fig, axs
