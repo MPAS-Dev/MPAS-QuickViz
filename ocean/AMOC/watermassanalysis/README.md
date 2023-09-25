25031169a66bb99ae8ce9291223e57ab742b8a46
bmooremaley <ben.mooremaley@gmail.com>
2023-09-25 16:09:46 -0700

    New streamlined workflow for the set of WMT analysis to date

--- ocean/AMOC/watermassanalysis/README.md
+++ ocean/AMOC/watermassanalysis/README.md
@@ -1,3 +1,7 @@
-# Watermass Analysis
+# Water Mass Analysis
 
-Development space for code and visualization related to watermass analysis in MPAS-Ocean.
+Development space for code and visualization related to water mass analysis in MPAS-Ocean.
+
+The primary method used in this analysis is the Walin air-sea flux framework ([Walin 1982, Tellus](https://doi.org/10.3402/tellusa.v34i2.10801); [Speer and Tziperman 1992](https://doi.org/10.1175/1520-0485(1992)022<0093:ROWMFI>2.0.CO;2)). Briefly, the mode water transformation $F$ and formation $M$ are defined in terms of the net air/sea density flux $\Phi$ area-integrated over and isopycnal outcrop.
+
+$$\underbrace{F(\rho) = \frac{\partial}{\partial\rho}\iint_{\rho_0}^{\rho}\Phi_{\rho}ds}_{\text{Transformation}} \hspace{1cm} \underbrace{M(\rho) = - \frac{\partial F}{\partial\rho}}_{\text{Formation}}$$
--- ocean/AMOC/watermassanalysis/modules/visualizationtools.py
+++ ocean/AMOC/watermassanalysis/modules/visualizationtools.py
@@ -8,12 +8,300 @@
 
 import numpy as np
 import xarray as xr
-from matplotlib import pyplot as plt, patches
+from matplotlib import pyplot as plt, patches, dates
 from datetime import datetime
-from tqdm import tqdm
 
 
-def parse_plot_args(ds, depths, months, sigmabins, clims):
+def slice_timeseries(da, region=None, sigmabins=None):
+    """Slice time series `xr.DataArray` by either region or sigmabins
+    """
+    
+    # Slice by region or sigmabins
+    if 'regionNames' in da.dims:
+        da = da.sel(regionNames=bytes(region, 'utf-8'))
+        if 'depths' in da.dims: da = da.sel(depths=0)
+    elif 'sigmaBins' in da.dims:
+        da = da.sel(sigmaBins=slice(*sigmabins)).sum(dim='sigmaBins')
+    
+    return da
+
+
+def build_lag_slices(lag):
+    """Build slices to stagger arrays for a given `lag`
+    """
+    
+    # Build slices
+    slice2 = slice(abs(lag), -(abs(lag) + 1))
+    slice1 = slice(slice2.start + lag, slice2.stop + lag)
+    
+    return [slice1, slice2]
+
+
+def calc_lag_correlation(variables, lags, cutoff=None):
+    """Calculate Pearson coefficient for each lag in `lags`
+    and return as `np.array`
+    """
+
+    # Loop through lags and calculate Pearson coefficient
+    r = []
+    for lag in lags:
+        slices = build_lag_slices(lag)
+        x, y = [var[slc].values for var, slc in zip(variables, slices)]
+        if cutoff is not None:
+            index = y > cutoff
+            x, y = x[index], y[index]
+        r.append(np.corrcoef(x, y)[0, 1])
+    r = np.array(r)
+
+    return r
+
+
+def plot_wmt_sigma(ds, varname, timeranges, months=None, ylims=None):
+    """Plot water mass transformation and formation in sigma space
+    time-averaged over each time range in `timeranges`.
+    `varname` specifies the grouping (heat, salt or total) and
+    `months` specifies which months to include in the time average.
+    """
+    
+    # Default months
+    if months is None:
+        months = list(range(1, 13))
+    if ylims is None:
+        ylims = [(None, None), (None, None)]
+    
+    # Make figure
+    fig, axs = plt.subplots(2, 1, figsize=(10, 5))
+    palette = plt.get_cmap('tab10')
+
+    # Loop through categories
+    for ax, ctgy, title, ylim in zip(axs, ['Trans', 'Form'], ['Transformation', 'Formation'], ylims):
+
+        # Loop through time ranges
+        for timerange, ls in zip(timeranges, [':', '-']):
+            tstr = '-'.join(str(t.year) for t in timerange)
+            
+            # Loop through meshes -> slice and time average -> plot
+            for mesh, c in zip(ds, palette([0, 1])):
+                da = ds[mesh][varname + ctgy].sel(time=slice(*timerange))
+                da = da.sel(time=da.time.dt.month.isin(months)).mean(dim='time')
+                ax.plot(da.sigmaBins, da, ls, color=c, label=f'{mesh} {tstr}')
+
+        # Format panel
+        ax.plot([21, 29], [0, 0], 'k--')
+        ax.set_xlim([21.5, 28.3])
+        ax.set_ylim(ylim)
+        ax.set_ylabel('Sv')
+        ax.set_title(title)
+
+    # Final formatting
+    axs[0].xaxis.set_ticklabels('')
+    axs[1].set_xlabel('$\sigma_{\\theta}$ [kg m$^{-3}$]')
+    axs[0].legend(loc=2)
+
+    return fig, axs
+
+
+def plot_wmt_timeseries(ds, varname, timeranges, sigmarange, ylim=[-100, 150]):
+    """Plot formation time series for the formation component
+    given by `varname` on the time ranges given by `timeranges`.
+    `sigmarange` designates the range of sigma values to integrate
+    over, determining the type of mode water in question.
+    """
+
+    # Make figure
+    meshes = list(ds)
+    fig, axs = plt.subplots(2, 2, figsize=(10, 5), gridspec_kw={'wspace': 0.05})
+    palette = plt.get_cmap('BrBG').resampled(4)
+    fcs, ecs = palette([1, 2]), palette([0, 3])
+
+    # Loop through meshes
+    for row, mesh in zip(axs, meshes):
+
+        # Initial formatting
+        row[0].set_ylabel('Sv')
+        row[0].text(0.01, 0.03, mesh, transform=row[0].transAxes)
+        row[1].yaxis.set_ticklabels('')
+
+        # Loop through time ranges
+        for ax, timerange in zip(row, timeranges):
+
+            # Panel formatting
+            ax.plot(timerange, [0, 0], 'k--')
+            ax.set_xlim(timerange)
+            ax.set_ylim(ylim)
+            ax.xaxis.set_major_locator(dates.YearLocator(base=2))
+            ax.xaxis.set_major_formatter(dates.DateFormatter('%Y'))
+            if mesh == meshes[0]:
+                ax.xaxis.set_ticklabels('')
+
+            # Slice variable by time range and sigmarange
+            da = ds[mesh][varname].sel(time=slice(*timerange)).sel(sigmaBins=slice(*sigmarange))
+
+            # Plot positive (formation) and negative (destruction) components
+            for label, func, fc, ec in zip(['Formation', 'Destruction'], ['greater', 'less'], fcs, ecs):
+                da_plot = da.where(getattr(np, func)(da, 0)).sum(dim='sigmaBins')
+                ax.fill_between(da_plot.time, da_plot, 0, fc=fc, ec=ec, alpha=0.6, label=label)
+
+            # Plot total (net) formation
+            da_plot = da.sum(dim='sigmaBins')
+            ax.plot(da_plot.time, da_plot, color='r', label='Net')
+
+    axs[1, 0].legend(ncols=3, loc=9)
+    
+    return fig, axs
+
+
+def plot_wmt_hoffmueller(ds, varname, timeranges, xlim=[25, 28], clim=75):
+    """Plot Hoffmueller time vs sigma figure for the WMT variable
+    given by 'varname' over the time ranges in `timeranges`.
+    """
+
+    # Make figure and add xlabel and titles
+    meshes = list(ds)
+    fig, axs = plt.subplots(2, 3, figsize=(10, 10), gridspec_kw={'hspace': 0.05, 'wspace': 0.1})
+    axs[1, 1].set_xlabel('$\sigma_{\\theta}$ [kg m$^{-3}$]')
+    for col, title in zip(axs.T, meshes + [f'{meshes[0]}-{meshes[1]}']):
+        col[0].set_title(title)
+
+    # Loop through time ranges
+    for row, timerange in zip(axs, timeranges[::-1]):
+
+        # Plot variable on time range for each mesh
+        res = []
+        for ax, mesh in zip(row, meshes):
+            da = ds[mesh][varname].sel(time=slice(*timerange))
+            c1 = ax.pcolormesh(da.sigmaBins, da.time, da, cmap='BrBG_r', vmin=-clim, vmax=clim)
+            res.append(da)
+
+        # Plot residual between meshes
+        res = np.subtract(*res)
+        c2 = row[2].pcolormesh(res.sigmaBins, res.time, res, cmap='RdBu_r', vmin=-clim, vmax=clim)
+
+        # Formatting
+        for n, ax in enumerate(row):
+            ax.set_xlim(xlim)
+            if n !=0:
+                ax.yaxis.set_ticklabels('')
+            if timerange == timeranges[1]:
+                ax.xaxis.set_ticklabels('')
+
+    # Add colorbars
+    cax1 = fig.add_axes([0.13, 0.04, 0.5, 0.01])
+    cax2 = fig.add_axes([0.66, 0.04, 0.23, 0.01])
+    fig.colorbar(c1, cax=cax1, label='Sv', orientation='horizontal')
+    fig.colorbar(c2, cax=cax2, label='Sv', orientation='horizontal')
+    for x, mesh in zip([0.9, 0.1], meshes):
+        cax2.text(x, 1.5, mesh, transform=cax2.transAxes, ha='center')
+    
+    return fig, axs
+
+
+def plot_lag_correlation(ax, ds, varnames, sigmabins, regions=None, cutoff=None, laglim=6):
+    """Plot the correlation vs lag as defined by the Pearson coefficient for
+    the variables given by `varnames`. `cutoff` defines the minimum value to
+    included for the dependent variable.
+    """
+    
+    # Define lags, meshes and default regions
+    lags = list(range(-laglim, laglim+1))
+    meshes = list(ds)
+    if regions is None:
+        regions = ds[meshes[0]].regionNames.values.astype(str)
+
+    # Figure formatting
+    palette = plt.get_cmap('tab10')
+    xlim, ylim = [-laglim, laglim], [-0.8, 1]
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    ax.plot(xlim, [0, 0], 'k--')
+    ax.plot([0, 0], ylim, 'k--')
+    ax.set_xlabel('Lag [months]')
+    ax.set_ylabel('Pearson coefficient')
+
+    # Loop through meshes
+    for mesh, c in zip(meshes, palette([0, 1])):
+        
+        # Slice variable 2
+        var2 = slice_timeseries(ds[mesh][varnames[1]], sigmabins=sigmabins)
+
+        # Loop through regions
+        r = []
+        for region in regions:
+
+            # Slice variable 1 and append Pearson coefficient
+            var1 = slice_timeseries(ds[mesh][varnames[0]], region=region)
+            r.append(calc_lag_correlation([var1, var2], lags, cutoff=cutoff))
+
+        # Calculate region stats
+        r = np.vstack(r)
+        r_mean = r.mean(axis=0)
+        
+        # Plot errorbar if multiple regions
+        if len(regions) == 1:
+            label, alpha = regions[0], 0.3
+        else:
+            r_err = [abs(getattr(r, func)(axis=0) - r_mean) for func in ('min', 'max')]
+            ax.errorbar(lags, r_mean, yerr=r_err, color=c, capsize=2)
+            label, alpha = mesh, 1
+        
+        # Plot mean
+        ax.plot(lags, r_mean, '-o', color=c, label=label, alpha=alpha)
+
+
+def plot_compare_variables(ax, ds, varnames, region=None, sigmabins=None, lims=None, lag=0):
+    """Compare MPAS timeseries variables given by `varnames` on x, y.
+    """
+    
+    # Define slices to account for lag, and add title
+    slices = build_lag_slices(lag)
+
+    # Loop through meshes
+    for mesh in ds:
+        
+        # Slice variables
+        variables = []
+        for varname, slc in zip(varnames, slices):
+            da = slice_timeseries(ds[mesh][varname], region=region, sigmabins=sigmabins)
+            variables.append(da[slc])
+        
+        # Plot
+        ax.plot(*variables, 'o', label=mesh)
+    
+    # Formatting
+    lims = [None, None, None, None] if lims is None else lims
+    ax.set_xlim(lims[:2])
+    ax.set_ylim(lims[2:])
+    ax.set_title(region)
+
+
+def plot_correlation_multipanel(ds, varnames, sigmabins, regionlist, xlabel, lims=None, lag=0, cutoff=0):
+    """Wrapper function around `plot_lag_correlation` and `plot_compare_variables`
+    for a multipanel correlation summary between the variables given by `varnames`.
+    """
+
+    # Make plot
+    fig = plt.figure(figsize=(10, 8))
+    gs = plt.GridSpec(2, 3, hspace=0.3, wspace=0.08)
+    props = {'fc': 'w', 'boxstyle': 'round', 'alpha': 0.7}
+
+    # Top panel
+    ax = fig.add_subplot(gs[0, :])
+    for regions in regionlist:
+        plot_lag_correlation(ax, ds, varnames, sigmabins, regions, cutoff)
+    ax.legend()
+
+    # Bottom panels
+    panelregions = ['Labrador Sea', 'Irminger Sea', 'Greenland Sea']
+    axs = [fig.add_subplot(gs[1, n]) for n in range(3)]
+    for ax, region in zip(axs, panelregions):
+        plot_compare_variables(ax, ds, varnames, region, sigmabins, lims, lag)
+        if region != panelregions[0]: ax.yaxis.set_ticklabels('')
+    axs[1].set_xlabel(xlabel)
+    axs[0].set_ylabel('SPMW Formation [Sv]')
+    axs[0].text(0.02, 0.03, f'Lag = {lag} months', transform=axs[0].transAxes, bbox=props)
+
+
+def parse_2Dplot_args(ds, depths, months, sigmabins, clims):
     """Parse arguments in the call to `plot_2Dvariable` into the
     needed variable types and assign defaults.
     """
@@ -91,7 +379,7 @@ def plot_2Dvariable(
     """
     
     # Parse plot arguments
-    meshes, tstrs, sliceargs, clims = parse_plot_args(ds_in, depths, months, sigmabins, clims)
+    meshes, tstrs, sliceargs, clims = parse_2Dplot_args(ds_in, depths, months, sigmabins, clims)
     
     # Animation frame labels
     if animdim == 'months':
