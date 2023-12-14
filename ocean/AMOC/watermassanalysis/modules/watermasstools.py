a7c0163c9b33df07418b87b87fc14bf1e49b5243
bmooremaley <ben.mooremaley@gmail.com>
2023-12-14 12:23:16 -0800

    Cleaned up basic_wmt_mpaso_example.ipynb in water mass analysis, and added 2D workflow

--- ocean/AMOC/watermassanalysis/modules/watermasstools.py
+++ ocean/AMOC/watermassanalysis/modules/watermasstools.py
@@ -38,37 +38,41 @@ def calc_state_variables(S, T, P=0):
     cpsw = 3.996e3    # Heat capacity of seawater [J kg-1 K-1]
 
     # Calculate state variables
-    statevars = {
-        'sigmaTheta': rho(S, T, P) - 1000,
-        'heatFactor': drhodt(S, T, P) / rho0 / cpsw,
-        'saltFactor': -drhods(S, T, P) / rho0 * S,
-    }
+    sigmaTheta = rho(S, T, P) - 1000
+    heatFactor = drhodt(S, T, P) / rho0 / cpsw
+    saltFactor = -drhods(S, T, P) / rho0 * S
 
-    return statevars
+    return sigmaTheta, heatFactor, saltFactor
 
 
 def build_combined_fluxes(
-    ds_in, statevars, prefix='timeMonthly_avg_',
+    ds_in, heatFactor, saltFactor, subdomain=None, prefix='timeMonthly_avg_',
     fluxdefs='../yaml/variable_combinations.yaml',
 ):
     """Build combined surface flux variables based on the combined definitions
     in the `vardefs` yaml file. Also build the buoyancy fluxes explicitly.
     """
     
+    # Define subdomain index
+    index = subdomain if subdomain is not None else slice(None, None)
+    
     # Open variable definitions
     with open(fluxdefs, 'r') as f:
         ctgys = yaml.safe_load(f)
     
     # Combine variables
-    fluxes = {ctgy: sum([ds_in[prefix + name][0, ...] for name in ctgys[ctgy]]) for ctgy in ctgys}
-    fluxes['buoyancyHeatFlux'] = statevars['heatFactor'] * fluxes['totalHeatFlux']
-    fluxes['buoyancySaltFlux'] = statevars['saltFactor'] * fluxes['totalSaltFlux']
+    fluxes = {ctgy: sum([ds_in[prefix + name][0, :].values[index] for name in ctgys[ctgy]]) for ctgy in ctgys}
+    fluxes['buoyancyHeatFlux'] = heatFactor * fluxes['totalHeatFlux']
+    fluxes['buoyancySaltFlux'] = saltFactor * fluxes['totalSaltFlux']
     fluxes['buoyancyTotalFlux'] = fluxes['buoyancyHeatFlux'] + fluxes['buoyancySaltFlux']
     
     return fluxes
 
 
-def calc_wmt(fluxes, sigmaTheta, areaCell=None, remapvars=None, sigmarange=[21, 29], binsize=0.1):
+def calc_wmt(
+    fluxes, sigmaTheta, coords, regions=None,
+    remapvars=None, sigmarange=[21, 29], binsize=0.1,
+):
     """Calculate time-averaged water mass transformation over specified sigma bins,
     remap to lon, lat and return in sigma-lonlat space as an `xr.Dataset`
     """
@@ -79,40 +83,79 @@ def calc_wmt(fluxes, sigmaTheta, areaCell=None, remapvars=None, sigmarange=[21, 
     # Define unit scale (1D: Sv, 2D: 1e-6 Sv km-2)
     scale = 1e-6 if remapvars is None else 1e6
     
-    # Initialize lists
-    data = {ctgy: [] for ctgy in ['heat', 'salt', 'total']}
+    # Initialize variables dict
+    ctgys = ['heat', 'salt', 'total']
+    variables = {ctgy + 'Transformation': [] for ctgy in ctgys}
+    
+    # Initialize lonlat if 2D
+    if remapvars is not None:
+        lon, lat = None, None
 
+    # ----------------------------------------------------------------------
     # Loop through sigmabins
     for sigmabin in sigmabins:
 
         # Create sigma mask
-        mask = (sigmaTheta >= sigmabin) & (sigmaTheta <= sigmabin + binsize)
+        sigmaMask = (sigmaTheta >= sigmabin) & (sigmaTheta <= sigmabin + binsize)
 
         # Loop through flux categories
-        for ctgy in data:
+        for ctgy in ctgys:
             
-            # Mask sigma and average over time, then assign nan to zero
-            da = fluxes[f'buoyancy{ctgy.capitalize()}Flux'].where(mask)
+            # Extract flux and apply sigma mask
+            flux = fluxes[f'buoyancy{ctgy.capitalize()}Flux'][sigmaMask]
             
-            if remapvars is None:  # 1D output
+            # --------------------------------------------------------------
+            # Return spatial transformation map (2D output)
+            if remapvars is not None:
                 
-                # Integrate over outcrop
-                da = (da * areaCell).sum(dim='nCells')
+                # Remap to lonlat, get coordinates and convert to numpy
+                transformation = pptools.remap(flux, coords['nCells'][sigmaMask], **remapvars)
+                if lon is None or lat is None:
+                    lon, lat = [transformation[name].values for name in ('lon', 'lat')]
+                transformation = transformation.values
+            
+            # --------------------------------------------------------------
+            # Integrate over sigma outcrop (1D output)
+            else:
+                
+                # Multiply by area
+                flux = flux * coords['area'][sigmaMask]
+                
+                # Calculate by region
+                if regions is not None:
+                    
+                    # Loop over regions
+                    transformation = []
+                    for region in regions:
+                        index, = np.where(coords['regionNames'] == region)
+                        regionMask = coords['regionMasks'][sigmaMask, index[0]]
+                        transformation.append(flux[regionMask].sum(axis=0))
+                
+                # Calculate global transformation only
+                else:
+                    transformation = flux.sum(axis=0)
 
-            else:  # 2D output
-                
-                # Time average -> replace nan with zero -> remap to lonlat
-                da = da.mean(dim='time')
-                da = pptools.remap(da.where(~np.isnan(da), 0), **remapvars)
-            
+            # --------------------------------------------------------------
             # Append to list
-            data[ctgy].append(da)
+            variables[ctgy + 'Transformation'].append(transformation)
 
-    # Concatenate DataArrays and calculate final quantities
-    wmt = {}
-    for ctgy in data:
-        transformation = xr.concat(data[ctgy], sigmabins) / binsize * scale
-        wmt[ctgy + 'Trans'] = transformation #.isel(sigmaBins=slice(0, -1))
-        #wmt[ctgy + 'Form'] = -transformation.diff(dim='sigmaBins', label='lower')
+    # ----------------------------------------------------------------------
+    # Prepare xarray output
+    #
+    # Define xr.Dataset coordinates
+    coordinates = {'sigmaBins': sigmabins}
+    if regions is not None:
+        coordinates['regionNames'] = regions
+    elif remapvars is not None:
+        coordinates['lat'] = lat
+        coordinates['lon'] = lon
     
-    return wmt
+    # Calculate final transformation and assign to tuple with dims
+    dims = coordinates.keys()
+    for name in variables:
+        variables[name] = (dims, np.array(variables[name]) / binsize * scale)
+    
+    # Build xr.Dataset
+    ds_out = xr.Dataset(variables, coordinates)
+    
+    return ds_out
