fc96623c17776ec628f62b160513846f332f8a81
Mark Petersen <mpetersen@lanl.gov>
2024-01-28 11:06:16 -0700

    add plot to alter_wind_stress

--- ocean/alter_initial_conditions/alter_wind_stress.py
+++ ocean/alter_initial_conditions/alter_wind_stress.py
@@ -15,6 +15,8 @@ newFileName = 'forcing_data_altered_winds.nc'
 
 import numpy as np
 import xarray
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 deg2rad = 3.14159/180.0
 rad2deg = 180.0/3.14159
@@ -31,10 +33,29 @@ windStressMeridional = forcing.variables['windStressMeridional']
 windStressZonal = forcing.variables['windStressZonal']
 
 print('before iCell loop')
+# initialize time zero
 windStressMeridional[0,:] = 0.0
 for iCell in range(nCells):
-    # initialize time zero
     windStressZonal[0,iCell] = 0.1 * np.cos(latCell[iCell]);
 
 forcing.to_netcdf(runDir+newFileName)
 
+enable_plotting = True
+if (enable_plotting):
+    print('save plot')
+    fig = plt.figure(figsize=(12,12))
+    varNames =['windStressZonal','windStressMeridional']
+    nCol = 1
+    nRow = 2
+    iVar = 0
+    for iRow in range(nRow):
+        for iCol in range(nCol):
+            var = forcing.variables[varNames[iVar]]
+            plt.subplot(nRow, nCol, iCol * nCol + iRow + 1)
+            plt.scatter(lonCell*rad2deg, latCell*rad2deg ,c=var.values, cmap='bwr', s=1)
+            plt.colorbar()
+            plt.axis('off')
+            plt.title(varNames[iVar])
+            iVar += 1
+    
+    plt.savefig('wind_forcing.png')
